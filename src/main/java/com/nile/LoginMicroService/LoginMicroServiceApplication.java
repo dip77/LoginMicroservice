021ec66fb6c6b285abbb767dc6166f9df8bcaeff
Dip Halani <diphalani@Dips-MacBook-Pro.local>
2018-06-12 17:37:03 +0530

    first commit

---
+++ src/main/java/com/nile/LoginMicroService/LoginMicroServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nile.LoginMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginMicroServiceApplication.class, args);
+	}
+}
